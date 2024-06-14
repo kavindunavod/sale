eafeae0703a515c9fc5072f9b90b875a3f57ec88
kavindunavod <140165157+kavindunavod@users.noreply.github.com>
2024-06-15 00:01:53 +0530

    Update project7.scala

--- project7.scala
+++ project7.scala
@@ -2,14 +2,20 @@
 object project7 {
 
   def main(args: Array[String]) : Unit = {
-    val price=24.95;
+  calculate();
+
+  }
+
+  def calculate(): Unit = {
+    val price = 24.95;
     val discount = 0.4;
     val book = 60;
-    val shipping_cost=3+0.75*10;
-    val quantity=60;
-    val total=(quantity*price)*discount+shipping_cost;
+    val shipping_cost = 3 + 0.75 * 10;
+    val quantity = 60;
+    val total = (quantity * price) * discount + shipping_cost;
     val rounded = (math.round(total * 100.0) / 100.0)
     println(rounded);
+
   }
 
 }
